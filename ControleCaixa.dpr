a7e6439a0e8359ba8ef959586417c28589da6d74
Diego Ferreira <diferreira.program@gmail.com>
2022-05-02 11:49:37 -0300

    main Layout

--- ControleCaixa.dpr
+++ ControleCaixa.dpr
@@ -1,14 +1,20 @@
 program ControleCaixa;
 
 uses
+  madExcept,
+  madLinkDisAsm,
+  madListHardware,
+  madListProcesses,
+  madListModules,
   System.StartUpCopy,
   FMX.Forms,
-  uMain in 'uMain.pas' {Form1};
+  uMain in 'uMain.pas' {FrmMain};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TFrmMain, FrmMain);
+  Application.RegisterFormFamily('Segoe UI', [TFrmMain]);
   Application.Run;
 end.
--- uMain.pas
+++ uMain.pas
@@ -4,10 +4,28 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, FMX.Objects, FMX.Effects,
+  FMX.ListView.Types, FMX.ListView.Appearances, FMX.ListView.Adapters.Base,
+  FMX.ListView;
 
 type
-  TForm1 = class(TForm)
+  TFrmMain = class(TForm)
+    LytMenus: TLayout;
+    LytSaldo: TLayout;
+    LblValorSaldo: TLabel;
+    Label3: TLabel;
+    img_menu: TImage;
+    Label1: TLabel;
+    ContainerCaixa: TRectangle;
+    Rectangle1: TRectangle;
+    Rectangle2: TRectangle;
+    Layout7: TLayout;
+    Label8: TLabel;
+    Label9: TLabel;
+    lv_lancamento: TListView;
+    Circle1: TCircle;
+    BtnAddVenda: TSpeedButton;
   private
     { Private declarations }
   public
@@ -15,7 +33,7 @@ type
   end;
 
 var
-  Form1: TForm1;
+  FrmMain: TFrmMain;
 
 implementation
 
